f895c0515fe21c80848182f62886563828c3480a
Aida <84194603+aidaneus@users.noreply.github.com>
2026-05-01 17:38:06 +0300

    second  commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1,1);
+        int b = calc.minus.apply(1, 1);
         int c = calc.devide.apply(a, b); //происходит деление на 0, поэтому возникает ошибка
 
         calc.println.accept(c);
